223a284f3578f1fd08d2cd6f771477f162b93275
Deepak <85782199+Deepak-96800@users.noreply.github.com>
2021-06-15 12:59:55 +0530

    Add files via upload

---
+++ Day-4 task.py
@@ -0,0 +1,41 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> #Create three variables (a,b,c) to same value of any integer & do the following
+>>> #a) Divide a by 10 b) Multiply b by 50 c) Add c value by 60
+>>> 
+>>> a=b=c=100
+>>> print("values of a =",a,"b=",b,"and c=",c)
+values of a = 100 b= 100 and c= 100
+>>> print("Divide a by 10 :",a/10)
+Divide a by 10 : 10.0
+>>> print("Multiply b by 50",b*50)
+Multiply b by 50 5000
+>>> print("Add c value by 60" ,c+60)
+Add c value by 60 160
+>>> 
+>>>  #Create a String variable of 5 characters and replace the 3rd character with G
+>>> 
+>>> name='PYTHON'
+>>> name
+'PYTHON'
+>>> print("after replacing G in string:",name[0:2]+'G'+name[3:6])
+after replacing G in string: PYGHON
+>>> 
+>>> #)Create two values (a,b) of int,float data type & convert thevise versa, Hint : convert a from int to float datatype & b from float to int datatype
+>>> 
+>>> a=15
+>>> b=15.7
+>>> print('a=',a)
+a= 15
+>>> print("After typecasting a to float =",float(a))
+After typecasting a to float = 15.0
+>>> print("b=",b)
+b= 15.7
+>>>  print("After typecasting a to int =",int(b))
+ 
+SyntaxError: unexpected indent
+>>> print("After typecasting a to int =",int(b))
+After typecasting a to int = 15
+>>> 
